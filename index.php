dcdb9162a498e62f9698c22b2cadef8c7eb44a00
karmeljuk <karmeljuk@gmail.com>
2013-10-24 19:22:41 +0300

    Class describes the first tent

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 		public $weight; //tent weight
 		public $type; //type of tent
 		public $reviews; //reviews on tent
+		public $price; //tent price
 		
 		abstract public function rating(points); //tent raiting
 		
@@ -20,7 +21,8 @@
 			$this->capacity = $arr['capacity'];
 			$this->weight 	= $arr['weight'];
 			$this->type 		= $arr['type'];
-			$this->reviews 	= $arr['reviews'];			
+			$this->reviews 	= $arr['reviews'];
+			$this->price 		= $arr['price'];			
 		}
 		
 		public function print_data()
@@ -30,6 +32,63 @@
 		}
 	
 	}
+	###
+	
+	#doc
+	#	classname:	Tent_TerraNova
+	#	scope:		PUBLIC
+	#	Class describes the first tent
+	#/doc
+	
+	class Tent_TerraNova extends Tent
+	{
+		#	internal variables
+	
+		#	Constructor
+		public function __construct ($arr)
+		{
+			$this->initialization($arr);
+		}
+		
+		public function rating(points)
+		{
+			if($rate == 5){
+				$this->reviews="good";
+			} else
+			{
+				$this->reviews="bad";
+			}
+			return $rate;
+		}
+	
+	}
+	###
+	
+	#doc
+	#	classname:	Tent_Campus
+	#	scope:		PUBLIC
+	#	Class describes the second tent
+	#/doc
+	
+	class Tent_Campus extends Tent
+	{
+		#	Constructor
+		public function __construct ($arr)
+		{
+			$this->initialization($arr);
+		}
+		
+		public function rating(points)
+		{
+			if ($rate > 3)
+			{
+				echo "<br> Not bad tent.";
+			}
+			return $rate;
+		}
+	
+	}
+	###
 	
 	
 
